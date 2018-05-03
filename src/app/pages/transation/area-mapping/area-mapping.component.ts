dd69d7b88774ff21abfaa09b5f6efe977b3cede9
ellappan <eellappan@gmail.com>
2018-05-03 21:40:03 +0530

    area mapping completed

--- src/app/pages/transation/area-mapping/area-mapping.component.ts
+++ src/app/pages/transation/area-mapping/area-mapping.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy} from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-area-mapping',
@@ -7,7 +8,118 @@ import { Component } from '@angular/core';
 })
 
 
-export class AreaMappingcomponent {
+export class AreaMappingcomponent  implements OnDestroy{
   starRate = 2;
   heartRate = 4;
+  themeName = 'default';
+  settings: Array<any>;
+  themeSubscription: any;
+  constructor(private themeService: NbThemeService) {
+    this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
+      this.themeName = theme.name;
+      this.init(theme.variables);
+    });
+  }
+  init(colors: any) {
+    this.settings = [{
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'New',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      },
+      {
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'Save',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      },
+      {
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'View',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      },
+      {
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'Modify',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      },
+      {
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'Refresh',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      },
+      {
+        class: 'btn-hero-secondary',
+        container: 'secondary-container',
+        title: 'Ghost Button',
+        buttonTitle: 'Cancel',
+        disable:'true',
+        default: {
+          border: '#dadfe6',
+        },
+        cosmic: {
+          border: colors.primary,
+          bevel: '#665ebd',
+          shadow: 'rgba (33, 7, 77, 0.5)',
+          glow: 'rgba (146, 141, 255, 1)',
+        },
+      }
+    ]}
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
 }
