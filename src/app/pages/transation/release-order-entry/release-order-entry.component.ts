bcc0391a47df56613381ea56f7bcfac45dc35ebc
ellappan <eellappan@gmail.com>
2018-05-10 21:15:08 +0530

    media

--- src/app/pages/transation/release-order-entry/release-order-entry.component.ts
+++ src/app/pages/transation/release-order-entry/release-order-entry.component.ts
@@ -12,10 +12,113 @@ import { NbThemeService } from '@nebular/theme';
 
 export class ReleaseOrderEntrycomponent implements OnDestroy{
     themeSubscription: any;
+    themeName = 'default';
+    settings: Array<any>;
     constructor(private themeService: NbThemeService) {
       this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
+        this.themeName = theme.name;
+        this.init(theme.variables);
       });
     }
+    init(colors: any) {
+      this.settings = [{
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: 'New',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        },
+        {
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: '  Save ',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        },
+        {
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: '  View ',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        },
+        {
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: ' Modify',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        },
+        {
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: 'Refresh',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        },
+        {
+          class: 'btn-hero-secondary',
+          container: 'secondary-container',
+          title: 'Ghost Button',
+          buttonTitle: 'Cancel ',
+          disable:'true',
+          default: {
+            border: '#dadfe6',
+          },
+          cosmic: {
+            border: colors.primary,
+            bevel: '#665ebd',
+            shadow: 'rgba (33, 7, 77, 0.5)',
+            glow: 'rgba (146, 141, 255, 1)',
+          },
+        }
+      ]}
+  
     ngOnDestroy() {
         this.themeSubscription.unsubscribe();
       }
